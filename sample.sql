1cadef4d04f4133c021d72628274fa206f1b35a1
kishorkumar-b <kishorkumar.b@agaramtech.com>
2025-09-17 17:38:37 +0530

    Add Employee SQL script demonstrating DDL, DML, DCL, and TCL with salary column and transaction fixes

--- sample.sql
+++ sample.sql
@@ -1,2 +1,78 @@
-SELECT @@VERSION;   -- shows SQL Server version
-SELECT GETDATE();   -- shows current date & time
+USE Shcooldb;
+GO
+/* for renaming */
+
+/* EXEC sp_rename 'Student.Program', 'Course', 'COLUMN'; */
+
+
+/* SELECT Name, Course
+FROM Student
+WHERE Age > 18; */
+
+/* INSERT INTO Student
+VALUES ('Ariya', 20, 'ECE'); */
+
+/* UPDATE Student
+SET Course = 'CSE'
+WHERE Name = 'Ravi'; */
+
+/* DELETE FROM Student
+WHERE Course = 'ECE'; */
+
+
+-- Run this in the master database creating new login server
+
+/* USE master;
+GO
+
+CREATE LOGIN MyLogin
+WITH PASSWORD = 'Kishor@123'; */
+
+--creating new user in mylogin
+
+/* CREATE USER MyUser FOR LOGIN MyLogin; */
+
+--grant permission
+
+/* GRANT DELETE
+ON Student
+TO MyUser; */
+
+/* -- swicth to my login
+EXECUTE AS USER = 'MyUser';
+GO
+
+USE Shcooldb;
+GO
+
+SELECT * 
+FROM Student;
+
+--This will fail DELETE was not granted
+DELETE FROM Student
+WHERE Name = 'Anbu';
+
+GO
+
+-- Return to original login
+REVERT;  */
+
+-- last granted permision is get removed
+
+/* REVOKE INSERT ON Student FROM MyUser; */
+
+/*  DENY DELETE ON Student to MyUser;  */
+
+-- transaction start
+BEGIN TRANSACTION
+
+UPDATE Student
+SET Course='ece'
+WHERE Name='Anbu';
+SAVE TRAN sp1; -- changes stored in the sp1 not permatly changed
+DELETE FROM Student WHERE Age=18;
+ROLLBACK TRAN sp1;  --roll back except spi
+COMMIT --end of transation
+
+
+SELECT * FROM Student
